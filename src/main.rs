a6e186523f573a17951049c6808790d35781b136
wzslr321 <wiktor.zajac888@gmail.com>
2021-06-30 23:38:15 +0200

    file reading error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use structopt::StructOpt;
+use anyhow::{Context, Result};
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    // Belissima
+    println!("{:?}", belissima());
+
+    // spaghetti
+    let result = std::fs::read_to_string(&args.path);
+    match result {
+        Ok(content) => {
+            for line in content.lines() {
+                if line.contains(&args.pattern) {
+                    println!("{}", line);
+                }
+            }
+        }
+        Err(error) => { println!("Oh no: {} ", error); }
+    }
+}
+
+fn belissima() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(&path).with_context(|| format!("Could not read the file `{}", path))?;
+    println!("File content: {}", content);
+    Ok(())
 }
