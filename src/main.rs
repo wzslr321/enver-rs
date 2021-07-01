eb463d67e81b3a65adb6a7dce8f069edcca2e5f2
wzslr321 <wiktor.zajac888@gmail.com>
2021-07-01 02:01:43 +0200

    opt struct specyfing command arguments

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
+use std::path::PathBuf;
 use structopt::StructOpt;
-use anyhow::{Context, Result};
+use std::home;
 
 #[derive(StructOpt)]
-struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+#[structopt(about = "Even the best sometimes need -h or --help...")]
+struct Opt {
+    /// Search for the specified variable, and modify if found
+    #[structopt(short, long)]
+    modify: bool,
+
+    /// Search for the specified variable, and delete if found
+    #[structopt(short, long)]
+    delete: bool,
+
+    /// Use specified variable name to create a new one
+    add: bool,
+
+    /// Require double confirmation to perform changes
+    #[structopt(long, default_value = true)]
+    confirm: bool,
+
+    /// Path to dotfile holding environment variables configuration.
+    #[structopt(long, parse(from_os_str), default = resolve_home_path())]
+    dot_path: PathBuf,
+
+    /// Environment variable name to look for
+    #[structopt(short, long, default = "PATH")]
+    var: String,
 }
 
 fn main() {
-    let args = Cli::from_args();
-
-    // Belissima
-    println!("{:?}", belissima());
-
-    // spaghetti
-    let result = std::fs::read_to_string(&args.path);
-    match result {
-        Ok(content) => {
-            for line in content.lines() {
-                if line.contains(&args.pattern) {
-                    println!("{}", line);
-                }
-            }
-        }
-        Err(error) => { println!("Oh no: {} ", error); }
-    }
 }
 
-fn belissima() -> Result<()> {
-    let path = "test.txt";
-    let content = std::fs::read_to_string(&path).with_context(|| format!("Could not read the file `{}", path))?;
-    println!("File content: {}", content);
-    Ok(())
+fn resolve_home_path() -> PathBuf {
+    home::home_dir()?
 }
